e94658f25f464817a3310e1aead6d7ace0c3b2d6
Naman Khandelwal <129101844+naman7727@users.noreply.github.com>
2024-12-13 10:15:19 +0530

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -13,7 +13,7 @@ const Project = () => {
                     </motion.div>
                     <motion.div whileInView={{ opacity: 1, x: 0 }} initial={{ opacity: 0, x: 100 }} transition={{ duration: 1 }} className='w-full max-w-xl lg:w-3/4'>
                         <h6 className='mb-2 font-semibold'>{Project.title}</h6>
-                        <p className='mb-4 text-neutral-400'>{Project.description}</p>
+                        <p className='mb-4 text-purple-500'>{Project.description}</p>
                         {Project.technologies.map((tech, index) => (
                             <span key={index} className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>{tech}</span>
                         ))}
